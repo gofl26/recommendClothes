8fedc3c5219c0079c1c12786995c8370c21df5cf
gofl26 <gofl26@naver.com>
2022-04-11 20:06:50 +0900

    cors 삭제

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connect = require('./schemas');
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -26,17 +26,17 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/index.html');
 });
 
-//CORS 테스트 (https://test-cors.org)
-const corsOption = {
-    origin: "http://52.78.194.238",
-    credentials: true,
-  };
+// //CORS 테스트 (https://test-cors.org)
+// const corsOption = {
+//     origin: "http://52.78.194.238",
+//     credentials: true,
+//   };
   
-app.use(cors(corsOption));
+// app.use(cors(corsOption));
 
-app.get("/cors-test", (req, res) => {
-res.send("hi");
-});
+// app.get("/cors-test", (req, res) => {
+// res.send("hi");
+// });
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 켜졌어요!');
