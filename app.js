117f09b648a0de2429e305653a10f98184a1e73b
mulbinbich <mulbinbich@gmail.com>
2022-04-13 19:06:40 +0900

    ({extended: true})변경, CORS테스트 변경

--- app.js
+++ app.js
@@ -18,29 +18,27 @@ const requestMiddleware = (req, res, next) => {
     next();
 };
 app.use(express.static('static'));
-app.use(express.json());
+//프론트에서 오는 데이터들을 body에 넣어주는 역할
+app.use(express.json()); 
 app.use(requestMiddleware);
 //multer 저장파일 조회
 app.use('/profile', express.static('uploads'));
 
+//form 형식으로 데이터를 받아오고 싶을 때(false->true)
 app.use('/api', express.urlencoded({ extended: false }), postsRouter);
-app.use('/api', express.urlencoded({ extended: false }), usersRouter);
+app.use('/api', express.urlencoded({ extended: true }), usersRouter);
 app.use('/api', express.urlencoded({ extended: false }), commentsRouter);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static/index.html');
 });
 
-// //CORS 테스트 (https://test-cors.org)
-// const corsOption = {
-//     origin: "http://52.78.194.238",
-//     credentials: true,
-//   };
-  
-
-// app.get("/cors-test", (req, res) => {
-// res.send("hi");
-// });
+//CORS 테스트 (https://test-cors.org)
+//origin: 'http://google.com', 구글에서만 데이터를 처리하는 것. 이래야 해커의 위협에서 보호가능
+//origin: '*', 어떤 곳에서든 데이터를 처리할 수 있게함. 
+// app.use(cors({
+//     orgin: '*',
+// }));
 
 app.listen(port, () => {
     console.log(port, '포트로 서버가 켜졌어요!');
