0d473a0e7e4da1fbca104c39a4258fc162f975b9
mulbinbich <mulbinbich@gmail.com>
2022-04-14 10:58:41 +0900

    extended 변경(서버 깃풀 오류)

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/profile', express.static('uploads'));
 
 //form 형식으로 데이터를 받아오고 싶을 때(false->true)
 app.use('/api', express.urlencoded({ extended: false }), postsRouter);
-app.use('/api', express.urlencoded({ extended: false }), usersRouter);
+app.use('/api', express.urlencoded({ extended: true }), usersRouter);
 app.use('/api', express.urlencoded({ extended: false }), commentsRouter);
 
 // app.get('/', (req, res) => {
